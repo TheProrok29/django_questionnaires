640b421c1d8b0331805c8aca5c5e8c7c774e035c
TheProrok29 <tomaszdbogacki@gmail.com>
2019-03-23 19:14:53 +0100

    Added views/path home and panel, create module accaounts

--- questionnaires/settings.py
+++ questionnaires/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'questionnaires.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'questionnaires/templates/')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
