1f4c214d04072fd342f065a4b7dd845777f6c75c
TheProrok29 <tomaszdbogacki@gmail.com>
2019-04-07 16:39:13 +0200

    Add notifications views
    *add notification view
    *add delete view
    *add set_as_read view
    *delete /questionares/__init__.py file

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import AnswerNotification
 
-# Create your views here.
+@login_required
+def notifications(request):
+    kwargs = {}
+    kwargs['notifications'] = AnswerNotification.objects.filter(user=request.user).order_by('read')
+    return render(request, 'notifications.html', kwargs)
+
+@login_required
+def delete(request, notification_id):
+    try:
+        notification = AnswerNotification.objects.get(id=notification_id)
+        notification.delete()
+        messages.success(request, 'Powiadomienie zostało usuniete')
+    except AnswerNotification.DoesNotExist:
+        messages.error(request, 'Wybrane powiadomienie nie istnieje')
+    return HttpResponseRedirect(reverse('notifications'))
+
+@login_required
+def set_as_read(request):
+    AnswerNotification.objects.filter(user=request.user).update(read=True)
+    messages.success(request, 'Operacja została wykonana')
+    return HttpResponseRedirect(reverse('notifications'))
+        
